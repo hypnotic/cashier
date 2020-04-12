a84d58bdcad6c622c703a28efeddde586bb587b6
Luis Freire <luis.freire@hypnotic.pt>
2020-04-13 00:56:44 +0100

    Add support for updating subscriptions table instead of recreating it

--- database/migrations/2019_05_03_000002_create_subscriptions_table.php
+++ database/migrations/2019_05_03_000002_create_subscriptions_table.php
@@ -13,20 +13,47 @@ class CreateSubscriptionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('subscriptions', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id');
-            $table->string('name');
-            $table->string('stripe_id');
-            $table->string('stripe_status');
-            $table->string('stripe_plan');
-            $table->integer('quantity');
-            $table->timestamp('trial_ends_at')->nullable();
-            $table->timestamp('ends_at')->nullable();
-            $table->timestamps();
+        if (Schema::hasTable('subscriptions')) {
+            Schema::table('subscriptions', function (Blueprint $table) {
+                if (!Schema::hasColumn('subscriptions', 'id')) {
+                    $table->bigIncrements('id');
+                }
+                if (!Schema::hasColumn('subscriptions', 'user_id')) {
+                    $table->unsignedBigInteger('user_id');
+                }
+                if (!Schema::hasColumn('subscriptions', 'name')) {
+                    $table->string('name');
+                }
 
-            $table->index(['user_id', 'stripe_status']);
-        });
+                $table->string('stripe_id');
+                $table->string('stripe_status');
+                $table->string('stripe_plan');
+                $table->integer('quantity');
+                $table->timestamp('trial_ends_at')->nullable();
+                $table->timestamp('ends_at')->nullable();
+
+                if (!Schema::hasColumn('subscriptions', 'created_at')) {
+                    $table->timestamps();
+                }
+
+                $table->index(['user_id', 'stripe_status']);
+            });
+        } else {
+            Schema::create('subscriptions', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedBigInteger('user_id');
+                $table->string('name');
+                $table->string('stripe_id');
+                $table->string('stripe_status');
+                $table->string('stripe_plan');
+                $table->integer('quantity');
+                $table->timestamp('trial_ends_at')->nullable();
+                $table->timestamp('ends_at')->nullable();
+                $table->timestamps();
+
+                $table->index(['user_id', 'stripe_status']);
+            });
+        }
     }
 
     /**
